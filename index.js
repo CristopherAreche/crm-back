099828a0b0b1d9aa3b3561f3bfe6f44ccb32942e
CristopherAreche <cristopherareche764@gmail.com>
2023-12-23 12:06:23 -0400

    backend

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 require("dotenv").config();
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
@@ -24,15 +5,9 @@ const cron = require("node-cron");
 const notifyTask = require("./src/controllers/tasks/notifyTask.js");
 
 cron.schedule("0 9 * * *", () => {
-  // SI COLOCAMOS * * * * * * se enviarian cada un segundo
-  // TODOS LOS DIAS A LAS 9 de la mañana
   notifyTask();
 });
-
-const port = process.env.PORT || 6972;
-// const port = 3000;
-
-// Syncing all the models at once.
+const port = process.env.DB_PORT || 5432;
 conn.sync().then(() => {
   server.listen(port, () => {
     console.log("--> 👌 Server Online in port " + port); // eslint-disable-line no-console
